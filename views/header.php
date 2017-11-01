0c803db8f43e1bc6fa45f03c695eb25f00c47fa9
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-10-31 18:32:19 -0600

    Initial commit

---
+++ views/header.php
@@ -0,0 +1,8 @@
+<header>
+	<div class="options">
+		<div class="option">1</div>
+		<div class="option">2</div>
+		<div class="option">3</div>
+		<div class="option">4</div>
+	</div>
+</header>
---
+++ views/index_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nombre del torneo</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+	<?php include 'header.php'; ?>
+</body>
+</html>
