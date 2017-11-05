c8c1c387adc03f23810496a47249d16b565bf49a
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-11-04 22:21:28 -0600

    maquetar aside y main

--- views/index_view.php
+++ views/index_view.php
@@ -8,5 +8,20 @@
 </head>
 <body>
 	<?php include 'header.php'; ?>
+
+	<div class="main-container">
+		<aside>
+			<ul class="aside-options">
+				<li>opci&oacute;n</li>
+				<li>opci&oacute;n</li>
+				<li>opci&oacute;n</li>
+				<li>opci&oacute;n</li>
+				<li>opci&oacute;n</li>
+			</ul>
+		</aside>
+		<main>
+				
+		</main>
+	</div>
 </body>
 </html>
